4b76a55ac2e12afa8853df48064ab6f36c30c66b
Van Philip Panugan <panuganvanphilip@gmail.com>
2022-01-20 22:05:58 +0800

    Added inventory.dart 🔥🔥

---
+++ lib/components/inventory.dart
@@ -0,0 +1,56 @@
+class InventoryList {
+  final List<PlantObj> listPlanObj;
+
+  InventoryList({
+    required this.listPlanObj,
+  });
+
+  factory InventoryList.fromRTDB(Map<String, dynamic> data) {
+    List<PlantObj> rawPlanObj = [];
+    for (var x in data.keys) {
+      final rawData = Map<String, dynamic>.from(data[x] as Map);
+      rawPlanObj.add(PlantObj.fromObj(rawData));
+    }
+    return InventoryList(listPlanObj: rawPlanObj);
+  }
+}
+
+class PlantObj {
+  final String careNeeds;
+  final String commonName;
+  final String description;
+  final String lightExposure;
+  final String photoLink;
+  final String plantID;
+  final int price;
+  final String scientificName;
+  final int stock;
+  final String waterUse;
+
+  PlantObj(
+      {required this.careNeeds,
+      required this.commonName,
+      required this.description,
+      required this.lightExposure,
+      required this.photoLink,
+      required this.plantID,
+      required this.price,
+      required this.scientificName,
+      required this.stock,
+      required this.waterUse});
+
+  factory PlantObj.fromObj(Map<String, dynamic> data) {
+    return PlantObj(
+      careNeeds: data['Care_Needs'] ?? "Null",
+      commonName: data['Common_Name'] ?? "Null",
+      description: data['Description'] ?? "Null",
+      lightExposure: data['Light_Exposure'] ?? "Null",
+      photoLink: data['Photo'] ?? "Null",
+      plantID: data['Plant_ID'] ?? 0,
+      price: data['Price'] ?? 0,
+      scientificName: data['Scientific_Name'] ?? "Null",
+      stock: data['Stock'] ?? 0,
+      waterUse: data['Water_Use'] ?? "Null",
+    );
+  }
+}
